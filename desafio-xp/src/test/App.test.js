6d5e19e651448b801abda23299c553b014ca8f41
Kamila Hydalgo <kamilahydalgo@gmail.com>
2022-07-20 18:56:00 -0300

    refactor: mudança de configuração para os testes

--- desafio-xp/src/test/App.test.js
+++ desafio-xp/src/test/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Login } from '../App';
+import Login from '../pages/Login/Login';
 
 test('renders learn react link', () => {
   render(<Login />);
